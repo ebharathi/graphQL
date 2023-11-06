c14dae0cf1f556e0e2bcd29de1d7637629b9c321
ebharathi <elayabharathi10102002@gmail.com>
2023-11-06 22:22:19 +0530

    added mongoDB con

---
+++ connection.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose')
+
+
+const connect=async()=>{
+    try {
+      const con=await mongoose.connect("mongodb+srv://projectone:projectone@cluster0.dzrb8.mongodb.net/2023?retryWrites=true&w=majority")
+      if(con)
+       console.log("Connection established.")
+    } catch (error) {
+         console.log("FAILED TO CONNECTED MONGODB")
+    }
+}
+module.exports={connect}
--- index.js
+++ index.js
@@ -1,26 +1,64 @@
 const express=require('express')
 const cors=require('cors');
+const bcrypt=require('bcryptjs')
 //import for graphQL
 const { graphqlHTTP } = require("express-graphql")
 const { buildSchema } = require("graphql")
+//mongoDB
+const {connect}=require('./connection')
+//schema
+const {User}=require('./schema')
+
 const app=express();
 app.use(express.json())
 app.use(cors())
+//mongoDB
+connect()
+//data
+const usersData=[
+    {
+        id:1,
+        name:"john",
+        age:21,
+        email:"john@gmail.com"
+    },
+    {
+        id:2,
+        name:"Mikel",
+        age:18,
+        email:"mikel@gmail.com"
+    }
+]
 //graphQL
 var schema = buildSchema(`
+  type User{
+     _id:ID
+     name:String
+     email:String
+     password:String
+  }
   type Query {
-    hello: String
+    user(_id:ID):User
+  }
+  type Mutation{
+    createUser(name:String,email:String,password:String):User
   }
 `)
-var root = {
-  hello: () => {
-    return "Hello world!"
-  },
+var resolver = {
+  createUser:async({name,email,password})=>{
+     const newUser=new User();
+     newUser.name=name;
+     newUser.email=email;
+     const hashedPassword=await bcrypt.hash(password,10);
+     newUser.password=hashedPassword;
+     const savedUser=await newUser.save();
+     return savedUser;
+  }
 }
 //connecting graphQL
 app.use('/graphql',graphqlHTTP({
     schema:schema,
-    rootValue:root,
+    rootValue:resolver,
     graphiql:true
 }))
 app.listen(5000,()=>{
